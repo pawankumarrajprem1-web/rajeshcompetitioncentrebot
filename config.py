ea20546003115e05d42e9755ed822db0eff4d4a7
MPC PAWAN <pawankumarrajprem1@gmail.com>
2026-09-04 22:32:49 +0530

    config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ import certifi
 from pymongo import MongoClient
 
 API_TOKEN = os.getenv("BOT_TOKEN", "")
-
 MONGO_URI = os.getenv("MONGO_URI", "")
 
-# टेंप्लेट फ़ाइलों के पाथ
+# ⚠️ अपनी Admin Telegram ID डालें (उदा: 123456789)
+ADMIN_ID = int(os.getenv("ADMIN_ID", ""))
+
 BASE_DIR = os.path.dirname(__file__)
 PPT_TEMPLATE = os.path.join(BASE_DIR, "template.pptx")
 DOCX_TEMPLATE = os.path.join(BASE_DIR, "template.docx")
 
-# MongoDB कनेक्शन
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where(), serverSelectionTimeoutMS=5000)
 db = client["rcc_quiz_db"]
 tests_col = db["test_papers"]
