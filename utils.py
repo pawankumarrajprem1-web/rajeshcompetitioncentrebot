d3b8158ad4789047f683f277409eb6f87e1a406a
MPC PAWAN <pawankumarrajprem1@gmail.com>
2026-09-06 00:06:15 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -162,10 +162,12 @@ def format_docx_option(label, opt_text, show_answer=False):
     
     # Green Tick ya Asterisk ko text se saaf karein
     cleaned = opt_text.replace("✅", "").replace("*", "").strip()
+    full_text = f"{label} {cleaned}"
     
+    # Agar Answer Test PDF hai aur ye sahi option hai, to kewal Bold hoga
     if show_answer and is_answer:
-        rt.add(f"{label} {cleaned}", bold=True, color="008000") # Answer mode me Bold aur Green
+        rt.add(full_text, bold=True)
     else:
-        rt.add(f"{label} {cleaned}", bold=False)
+        rt.add(full_text, bold=False)
         
     return rt
