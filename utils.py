b4a72dde2c6494726d55a4be167251f992ccdeb1
MPC PAWAN <pawankumarrajprem1@gmail.com>
2026-09-06 00:32:33 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -160,14 +160,12 @@ def format_docx_option(label, opt_text, show_answer=False):
     rt = RichText()
     is_answer = "✅" in opt_text or "*" in opt_text
     
-    # Tick mark aur Star ko text se saaf karein
+    # Green Tick ya Asterisk ko text se saaf karein
     cleaned = opt_text.replace("✅", "").replace("*", "").strip()
-    full_text = f"{label} {cleaned}"
     
-    # Agar Answer mode ON hai aur ye sahi option hai, to kewal Bold hoga
     if show_answer and is_answer:
-        rt.add(full_text, bold=True)
+        rt.add(f"{label} {cleaned}", bold=True, color="008000") # Answer mode me Bold aur Green
     else:
-        rt.add(full_text, bold=False)
+        rt.add(f"{label} {cleaned}", bold=False)
         
     return rt
