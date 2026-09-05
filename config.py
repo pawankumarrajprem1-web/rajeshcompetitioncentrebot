1c6aa8118d1befb656b33946417080c3bcf0213a
MPC PAWAN <pawankumarrajprem1@gmail.com>
2026-09-05 13:13:09 +0530

    config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 import certifi
 from pymongo import MongoClient
 
-API_TOKEN = os.getenv("BOT_TOKEN", "8951859856:AAG5teDA8nM6_sY7y6b_prOkT9tDXaSh5Iw")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mpcpawan:RswOqZ4uy3UQtM3Q@cluster0.edkvmpu.mongodb.net/")
+API_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "")
 
 # Admin ID Safe Parsing
-admin_id_raw = os.getenv("ADMIN_ID", "5772540382")
+admin_id_raw = os.getenv("ADMIN_ID", "")
 ADMIN_ID = int(admin_id_raw) if admin_id_raw.isdigit() else 0
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
